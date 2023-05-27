5c28c2fa8c440c7aa93436be13645f6129e3dcb4
thelonewolf123 <harishkumar.vellore@gmail.com>
2023-05-28 01:10:36 +0530

    refactor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,55 @@
-import { Tasks } from './Tasks'
+import { useCallback, useEffect, useState } from 'react'
 
 function App() {
+    const [task, setTask] = useState('')
+    const [tasks, setTasks] = useState<string[]>([])
+
+    const fetchTasks = useCallback(() => {
+        fetch('http://localhost:8000/tasks')
+            .then((r) => r.json())
+            .then((x) => {
+                setTasks(x)
+            })
+    }, [])
+
+    useEffect(() => {
+        fetchTasks()
+    }, [fetchTasks])
+
     return (
         <>
-            <Tasks />
+            <div className="flex">
+                <input
+                    type="text"
+                    placeholder="task"
+                    onChange={(e) => setTask(e.target.value)}
+                    value={task}
+                ></input>
+                <button
+                    onClick={() => {
+                        fetch('http://localhost:8000/task/add', {
+                            method: 'POST',
+                            body: JSON.stringify({ task }),
+                            headers: {
+                                'Content-Type': 'application/json'
+                            }
+                        })
+                            .then((r) => r.json())
+                            .then((d) => {
+                                console.log('Task added! ', d)
+                                setTask('')
+                                fetchTasks()
+                            })
+                    }}
+                >
+                    submit
+                </button>
+            </div>
+            <ul>
+                {tasks.map((z) => {
+                    return <li>{z}</li>
+                })}
+            </ul>
         </>
     )
 }
